334e97adab43bdc36d1bcdcdcc9f8e48a49e1c42
BradMW <bodell94@yahoo.com>
2021-09-30 22:18:55 -0500

    Got the consol to log txt input with save button. still need to get it to display

--- script.js
+++ script.js
@@ -2,16 +2,99 @@
 var timeDisplayEl = $('#time-display');
 var scheduler = document.getElementById('.container');
 var timeRow = document.getElementById('.myRow');
-var save = document.getElementById('saveBtn');
+var save = document.querySelectorAll('.saveBtn');
 var timeColor; 
-var modal = document.getElementById('myModal');
-var close = document.getElementsByClassName("btn-secondary")[0];
-var modal = $('myModal');
-var text = document.querySelector("txtinput");
+// // var modal = document.getElementById('myModal');
+// var close = document.getElementsByClassName("btn-secondary")[0];
+// var modal = $('myModal');
+var text = document.querySelectorAll(".txtinput");
+
+// event.preventDefault();
+
+$(".time-block").each(function() {
+  console.log(typeof $(this).attr('id'))
+
+console.log(typeof moment().hours());
+var timeofDay = Number($(this).attr('id'));
+
+console.log(timeofDay);
+
+  if (moment().hours() > timeofDay) {
+    $(this).addClass('past');
+  } else if (moment().hours() === timeofDay) {
+    $(this).removeClass('past');
+    $(this).addClass('present');
+  } else {
+    $(this).removeClass('present');
+    $(this).addClass('future');
+  }
+})
 
 
 
-//create a way to save the text to local storage 
+
+
+
+
+// save.addEventListener("click", function() {
+//   var savetxt = {
+//     text: text.value.trim()
+// };
+
+//   localStorage.setItem("save", JSON.stringify(text));
+// });
+
+// function renderMessage() {
+    
+// }
+
+
+
+// save.addEventListener("click", function() {
+//   console.log(save);
+//   var comment = document.querySelector(".txtinput").value;
+//   localStorage.setItem("comment", comment);
+//   console.log(localStorage.setItem);
+// })
+
+// create a way to save the text to local storage 
+
+$('.saveBtn').each(function() {
+  console.log($(this));
+  // console.log(event.target);
+
+var saveTarget = $(this);
+
+saveTarget.on("click", function(event) {
+  event.preventDefault();
+
+
+console.log(text);
+console.log($(this).siblings().eq(1).val());
+
+  var user = {
+    save: $(this).siblings().eq(1).val().trim(),
+    
+    };
+
+      localStorage.setItem("user", JSON.stringify(user));
+});
+
+
+})
+
+$('.txtinput').each(function() {
+ 
+  
+ var inputTxt = JSON.parse(localStorage.getItem("user"));
+ $(this).text(inputTxt);
+ console.log(inputTxt.save);
+ $(this)
+
+})
+
+
+
 // save.addEventListener("click", function(event) {
 // var user = {
 //     save: text.value.trim(),
@@ -46,12 +129,12 @@ function displayTime() {
 //     modal.style.display = "block";
 //   }
 
-  close.onclick = function() {
-    modal.style.display = "none";
-  }
+  // close.onclick = function() {
+  //   modal.style.display = "none";
+  // }
 
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
